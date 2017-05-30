86355a20e2a23009be1b6e718cff9d23fb74f190
Shaurya <shahi.shaurya@gmail.com>
2017-05-30 14:24:31 +0530

    initial commit

---
+++ entertainment_center.py
@@ -0,0 +1,8 @@
+import media
+import fresh_tomatoes
+
+toy_story = media.Movie("Toy Story","A story about a boy and his toys that come to life","http://www.impawards.com/1995/posters/toy_story_ver1_xlg.jpg","https://www.youtube.com/watch?v=KYz2wyBy3kc")
+
+
+movies = [toy_story,toy_story,toy_story,toy_story,toy_story,toy_story]
+fresh_tomatoes.open_movies_page(movies)
